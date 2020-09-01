8dd309426ecfc70983b1bef76b4cc8e4f5e77c43
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-09-01 15:52:54 +0100

    working login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Switch } from "react-router-dom";
+import LoginView from './views/auth/LoginView'
+import LookUpView from './views/lookup/LookUpView'
+import { useAuth0 } from "@auth0/auth0-react";
+import history from "./utils/history";
+
 import './App.css';
 
 function App() {
+	const { isLoading, error } = useAuth0();
+
+	if(error) {
+		return (
+			<>
+			OPPS.. {error.message}
+			</>
+		)
+	}
+
+	if(isLoading) {
+		return <h3>Loading...</h3>
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Router history = {history}>
+			<Switch>
+				<Route path='/' exact component={ LoginView}  />
+				<Route path='/lookup' component={ LookUpView } />
+			</Switch>
+   
+
+		</Router>
   );
 }
 
