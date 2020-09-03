256429b2dccbe8fbc8c49ffde2d294e2b4d593c1
Magnus Sagerup <magnus.sagerup@gmail.com>
2020-09-03 09:22:31 +0100

    chained axios calls working

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 const Excel = require('exceljs');
 const xl = require('excel4node');
 const path = require('path');
+const { arrayIntersectSafe } = require('excel4node/distribution/lib/utils');
 
 
 // Set file storage location.
@@ -22,37 +23,44 @@ const upload = multer({storage: storage})
 
 // Get file fom client
 routes.post('/api/file', upload.single('file'), (req, res) => {
-	console.log('File name', req.file)
 	// Read file.
 	readXlsxFile(req.file.path).then((rows) => {
 		const orgNumberArr = rows;
 		return orgNumberArr;
 		// Then call brrg api with reg numbers. 
 	}).then(orgNumberArr => {
-		console.log(orgNumberArr)
-
-		// TODO: remove arrays in arrays, change to one array
-		// Chain axios calls.
-		// If time: Error check on org numbers.
-
-
-		// Enough for today. figure out how to loop overr... 
-
-		axios.all(
-			axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgNumberArr[0][0]}`),
-			axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgNumberArr[1][0]}`),
-			axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgNumberArr[2][0]}`),
-		)
-
-		axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgNumberArr[2][0]}`)
-		.then(responce => {
-			console.log(responce.data)
-			const info = responce.data;
-			res.send(info)
-		})
+		// flatten array.
+		const orgNumberFlatArr = [].concat.apply([], orgNumberArr);
+		console.log(orgNumberFlatArr)
+		// Chain Axios call based on number of elemets in array.
+		let orgInfo = [];
+		let promises = [];
+		let errors = [];
+		for(i = 0; i < orgNumberFlatArr.length; i++) {
+			promises.push(
+				axios.get(`https://data.brreg.no/enhetsregisteret/api/enheter/${orgNumberFlatArr[i]}`)
+				.then(res => {
+					orgInfo.push(res.data)
+				})
+				.catch(error => {
+					errors.push(error);
+					console.log(error)
+				})
+			)
+		}
+		
+		// Resolve all axios promises.
+		Promise.all(promises)
+			.then(() => {
+				// Send info to client side.
+				console.log(orgInfo)
+				res.status(200).json({orgInfo})
+			})
+			.catch(error => {
+				console.log(error)
+			})
 	})
-})
-
+});
 
 
 module.exports = routes;
